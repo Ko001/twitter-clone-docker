96b98dc839a4caf94b1e80bc7d39635c4bded18f
Ko001 <67585038+Ko001@users.noreply.github.com>
2021-02-05 21:47:41 +0900

    edit and destroy commetns

--- backend/app/Http/Controllers/CommentController.php
+++ backend/app/Http/Controllers/CommentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\UpdateComment;
 use App\Comment;
 use App\Tweet;
 use Auth;
@@ -18,5 +19,23 @@ class CommentController extends Controller
         $tweet->comments()->save($comment);
 
         return redirect()->route('tweets.show', ['tweet' => $tweet->id]);
+    }
+
+    public function edit(Tweet $tweet, Comment $comment)
+    {
+        return view('comments.edit', ['tweet' => $tweet, 'comment' => $comment]);
+    }
+
+    public function update(Tweet $tweet, Comment $comment, UpdateComment $request)
+    {
+        $comment->body = $request->body;
+        $tweet->comments()->save($comment);
+
+        return redirect()->route('tweets.show', ['tweet' => $tweet]);
+    }
+    public function destroy(Tweet $tweet, Comment $comment) 
+    {
+        $comment->delete();
+        return redirect()->route('tweets.show', ['tweet' => $tweet]);
     }
 }
