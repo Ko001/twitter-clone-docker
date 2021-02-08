cef0662e700d2e5cf0dff24233a5c12f365d7e3c
Ko001 <67585038+Ko001@users.noreply.github.com>
2021-02-09 02:16:34 +0900

    add icon for login toggle

--- backend/resources/views/layouts/app.blade.php
+++ backend/resources/views/layouts/app.blade.php
@@ -31,7 +31,7 @@
                     
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
+                <i class="fas fa-angle-double-down"></i>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
