563c73866b6eeba684514b734a1346425ca05956
Ko001 <67585038+Ko001@users.noreply.github.com>
2021-02-09 09:08:47 +0900

    add 419 view

---
+++ backend/resources/views/errors/419.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="container">
+    <div class="row">
+      <div class="col col-md-offset-3 col-md-6">
+        <div class="text-center">
+          <p>セッションがエクスパイヤーしました。</p>
+          <a href="{{ route('tweets.index') }}" class="">
+            ホームへ戻る
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
